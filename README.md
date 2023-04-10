318e9cd1bb9430efed2fdb6565940d320e6376eb
Denis Kazakov <denis@kazakov.ru.net>
2023-04-11 00:59:13 +0300

    add chunk dissambling

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # clox
+Clox is a general-purpose dynamically typed language with an automatic memory management.
+"C" in "Clox" means it is implemented in C language.  
+Inspired and written with [Robert Nystrom's "Crafting interpreters"](http://craftinginterpreters.com/).
--- src/main.c
+++ src/main.c
@@ -1,7 +1,17 @@
-int main(int argc, char** argv) {
-    if (argc >= 2)
-        return 0;
-    if (argv)
-        return 0;
-    return 0;
+#include "common.h"
+#include "chunk.h"
+#include "debug.h"
+
+int main(int argc, const char** argv) {
+	if (argc > 2)
+		// too many args
+		return 1;
+
+  Chunk chunk;
+  initChunk(&chunk);
+  writeChunk(&chunk, OP_RETURN);
+	disassembleChunk(&chunk, "test chunk");
+  freeChunk(&chunk);
+
+	return 0;
 }
